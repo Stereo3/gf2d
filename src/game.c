cb448b19e58ad252aa4ca4a118052adc9c54077c
Russell <rlohinau@gmail.com>
2024-03-13 17:04:34 -0400

    caught up on all the code

--- src/game.c
+++ src/game.c
@@ -3,30 +3,30 @@
 
 #include "gf2d_graphics.h"
 #include "gf2d_sprite.h"
+
+#include "font.h"
+#include "camera.h"
 #include "entity.h"
 #include "player.h"
 #include "world.h"
-
-
-//This is the beginning of something (hopefully) awesome
-//I love new branches <3
 
 int main(int argc, char * argv[])
 {
     /*variable declarations*/
     int done = 0;
     const Uint8 * keys;
+    World *world;
+    
     int mx,my;
     float mf = 0;
     Sprite *mouse;
     Color mouseColor = gfc_color8(255,100,255,200);
     Player *player;
-    World *world;
+    TextLine fps;
     
-    //backup
     /*program initializtion*/
     init_logger("gf2d.log",0);
-    slog("---==== BEGIN ====---");
+    slog("---==== BEGIN %s ====---",argv[0]);
     gf2d_graphics_initialize(
         "gf2d",
         1200,
@@ -35,25 +35,29 @@ int main(int argc, char * argv[])
         720,
         vector4d(0,0,0,255),
         0);
-    gf2d_graphics_set_frame_delay(16);
+    gf2d_graphics_set_frame_delay(33);
     gf2d_sprite_init(1024);
-    entity_system_init(2048);
+    font_init();
+    entity_system_init(1024);
     SDL_ShowCursor(SDL_DISABLE);
+    camera_set_size(vector2d(1200,720));
     
     /*demo setup*/
     mouse = gf2d_sprite_load_all("images/pointer.png",32,32,16,0);
-    player = player_new("Greg");
-    world = world_test_new();
+    player = player_new("greg");
+    world = world_load("maps/testworld.map");
+    world_setup_camera(world);
     /*main game loop*/
     while(!done)
     {
         SDL_PumpEvents();   // update SDL's internal event structures
         keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
+        font_cleanup();
         /*update things here*/
         SDL_GetMouseState(&mx,&my);
         mf+=0.1;
         if (mf >= 16.0)mf = 0;
-
+        
         entity_think_all();
         entity_update_all();
         player_think(player);
@@ -61,11 +65,15 @@ int main(int argc, char * argv[])
         
         gf2d_graphics_clear_screen();// clears drawing buffers
         // all drawing should happen betweem clear_screen and next_frame
-            //backgrounds drawn first
+            //backgrounds drawn first            
             world_draw(world);
-
+        
             entity_draw_all();
-    
+            
+            font_draw_text("Press ESC to quit",FS_small, GFC_COLOR_CYAN,vector2d(10,10));
+            gfc_line_sprintf(fps,"FPS: %f",gf2d_graphics_get_frames_per_second());
+            font_draw_text(fps,FS_small,GFC_COLOR_GREEN,vector2d(10,35));
+
             //UI elements last
             gf2d_sprite_draw(
                 mouse,
@@ -78,20 +86,13 @@ int main(int argc, char * argv[])
                 (int)mf);
 
         gf2d_graphics_next_frame();// render current draw frame and skip to the next frame
-
-        /* gfc_block_sprintf(tempStats
-        ,"Health: %i | Money: %i"
-        ,health
-        ,money); */
-
-        //gf2d_font_draw_line_tag(tempStats,FT_H1,gfc_color(1,0,1,1), vector2d(300,350));
         
         if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
         //slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
     }
     player_free(player);
     world_free(world);
-    slog("---==== END ====---");
+    slog("---==== END %s ====---", argv[0]);
     return 0;
 }
 /*eol@eof*/
--- src/world.c
+++ src/world.c
@@ -1,17 +1,160 @@
-#include "simple_json.h"
 #include "simple_logger.h"
+#include "simple_json.h"
+
+#include "gf2d_graphics.h"
+
+#include "camera.h"
 #include "world.h"
+
+void world_tile_layer_build(World *world)
+{
+    int i,j;
+    Vector2D position;
+    Uint32 frame;
+    Uint32 index;
+    if (!world)return;
+    
+    if (!world->tileSet)return;
+    
+    if (world->tileLayer)
+    {
+        gf2d_sprite_free(world->tileLayer);
+    }
+    
+    world->tileLayer = gf2d_sprite_new();
+    
+    world->tileLayer->surface = gf2d_graphics_create_surface(
+        world->tileWidth * world->tileSet->frame_w,
+        world->tileHeight * world->tileSet->frame_h);
+        
+    world->tileLayer->frame_w = world->tileWidth * world->tileSet->frame_w;
+    world->tileLayer->frame_h = world->tileHeight* world->tileSet->frame_h;
+    
+    if (!world->tileLayer->surface)
+    {
+        slog("failed to create tileLayer surface");
+        return;
+    }
+    
+    for (j = 0; j < world->tileHeight;j++)
+    {
+        for (i = 0; i < world->tileWidth;i++)
+        {
+            index = i + (j*world->tileWidth);
+            if (world->tileMap[index] == 0)continue;
+
+            position.x = i*world->tileSet->frame_w;
+            position.y = j*world->tileSet->frame_h;
+            frame = world->tileMap[index] - 1;
+
+            gf2d_sprite_draw_to_surface(
+                world->tileSet,
+                position,
+                NULL,
+                NULL,
+                frame,
+                world->tileLayer->surface);
+        }
+    }
+    world->tileLayer->texture = SDL_CreateTextureFromSurface(gf2d_graphics_get_renderer(),world->tileLayer->surface);
+    if (!world->tileLayer->texture)
+    {
+        slog("failed to convert world tile layer to texture");
+        return;
+    }
+}
+
+World *world_load(const char *filename)
+{
+    World *world = NULL;
+    SJson *json = NULL;
+    SJson *wjson = NULL;
+    SJson *vertical,*horizontal;
+    SJson *item;
+    int tile;
+    int w = 0,h = 0;
+    int i,j;
+    const char *tileSet;
+    const char *background;
+    int frame_w,frame_h;
+    int frames_per_line;
+    if (!filename)
+    {
+        slog("no filename provided for world_load");
+        return NULL;
+    }    
+    json = sj_load(filename);
+    if (!json)
+    {
+        slog("failed to load world file %s",filename);
+        return NULL;
+    }
+    wjson = sj_object_get_value(json,"world");
+    if (!wjson)
+    {
+        slog("%s missing 'world' object",filename);
+        sj_free(json);
+        return NULL;
+    }
+    vertical = sj_object_get_value(wjson,"tileMap");
+    if (!vertical)
+    {
+        slog("%s missing 'tileMap'",filename);
+        sj_free(json);
+        return NULL;
+    }
+    h = sj_array_get_count(vertical);
+    horizontal = sj_array_get_nth(vertical,0);
+    w = sj_array_get_count(horizontal);
+    world = world_new(w, h);
+    if (!world)
+    {
+        slog("failed to create space for a new world for file %s",filename);
+        sj_free(json);
+        return NULL;
+    }
+    for (j = 0; j < h;j++)
+    {
+        horizontal = sj_array_get_nth(vertical,j);
+        if (!horizontal)continue; //this might be worth erroring over, but for now just skip
+        for (i = 0; i < w;i++)
+        {
+            item = sj_array_get_nth(horizontal,i);
+            if (!item)continue;
+            tile = 0;
+            sj_get_integer_value(item,&tile);
+            world->tileMap[i+(j*w)] = tile;
+        }
+    }
+    background = sj_object_get_value_as_string(wjson,"background");
+    world->background = gf2d_sprite_load_image(background);
+    
+    tileSet = sj_object_get_value_as_string(wjson,"tileSet");
+    sj_object_get_value_as_int(wjson,"frame_w",&frame_w);
+    sj_object_get_value_as_int(wjson,"frame_h",&frame_h);
+    sj_object_get_value_as_int(wjson,"frames_per_line",&frames_per_line);
+    world->tileSet = gf2d_sprite_load_all(
+        tileSet,
+        frame_w,
+        frame_h,
+        frames_per_line,
+        1);
+    world_tile_layer_build(world);
+
+    sj_free(json);
+    return world;
+}
 
 World *world_test_new()
 {
     int i;
-    int width = 65, height = 45;
+    int width = 75,height = 45;
     World *world;
-
-    world = world_new(width, height);
-
-    if(!world)return NULL;
-
+    
+    world = world_new(width,height);
+    
+    if (!world)return NULL;
+    
     world->background = gf2d_sprite_load_image("images/backgrounds/bg_flat.png");
     world->tileSet = gf2d_sprite_load_all(
         "images/backgrounds/tileset.png",
@@ -19,81 +162,81 @@ World *world_test_new()
         16,
         1,
         1);
-    for(i = 0; i < width; i++)
+    for (i = 0; i < width;i++)
     {
         world->tileMap[i] = 1;
-        world->tileMap[(i)+((height - 1)*width)] = 1;
-        slog("Width: %i", width);
-    }    
-    for(i = 0; i < height; i++)
+        world->tileMap[i+((height -1 )*width)] = 1;
+    }
+    for (i = 0; i < height;i++)
     {
         world->tileMap[i*width] = 1;
-        world->tileMap[(i*width)+(width-1)] = 1;
-        slog("height: %i", height);
+        world->tileMap[i*width + (width - 1)] = 1;
     }
+    world_tile_layer_build(world);
     return world;
 }
 
 World *world_new(Uint32 width, Uint32 height)
 {
     World *world;
-
-    if((!width)||(!height))
+    
+    if ((!width)||(!height))
     {
-        slog("Cannot make a world with zero width and height");
+        slog("cannot make a world with zero width and height");
         return NULL;
     }
-
+    
     world = gfc_allocate_array(sizeof(World),1);
-    if(!world)
+    if (!world)
     {
         slog("failed to allocate a new world");
         return NULL;
     }
-
-    world->tileMap = gfc_allocate_array(sizeof(Uint8), height*width);
+    // all boilerplate code would go here
+    // all defaults
+    world->tileMap = gfc_allocate_array(sizeof(Uint8),height*width);
     world->tileHeight = height;
     world->tileWidth = width;
+    slog("world created with width %i, height %i",width,height);
     return world;
 }
 
 void world_free(World *world)
 {
-    if(!world)return;
-
+    if (!world)return;
+    
     gf2d_sprite_free(world->background);
     gf2d_sprite_free(world->tileSet);
+    gf2d_sprite_free(world->tileLayer);
     free(world->tileMap);
     free(world);
 }
 
+//  000000000000000000000000
+//  000000000000000000000000
+//  000000000000000000000000
+//  000000000000000000000000
+
 void world_draw(World *world)
 {
-    int i,j;
-    int index;
-    int frame;
-    Vector2D position;
-    if(!world)return;
+    Vector2D offset;
+    if (!world)return;
+    offset = camera_get_offset();
     gf2d_sprite_draw_image(world->background,vector2d(0,0));
-    if(!world->tileSet)return;
-    for(j = 0;j < world->tileHeight; j++)
-    {
-        for(i = 0;i < world->tileWidth; i++)
-        {
-            index = i + (j*world->tileWidth);
-            if(world->tileMap[index] == 0)continue;
-            position.x = i*world->tileSet->frame_w;
-            position.y = i*world->tileSet->frame_h;
-            frame = world->tileMap[index] - 1;
-            gf2d_sprite_draw(
-                world->tileSet,
-                position,
-                NULL,
-                NULL,
-                NULL,
-                NULL,
-                NULL,
-                frame);
-        }
-    }
+    gf2d_sprite_draw_image(world->tileLayer,offset);
 }
+
+void world_setup_camera(World *world)
+{
+    if (!world)return;
+    if ((!world->tileLayer)||(!world->tileLayer->surface))
+    {
+        slog("no tile layer set for world");
+        return;
+    }
+    camera_set_bounds(gfc_rect(0,0,world->tileLayer->surface->w,world->tileLayer->surface->h));
+    camera_apply_bounds();
+    camera_enable_binding(1);
+}
+
+/*eol@eof*/
